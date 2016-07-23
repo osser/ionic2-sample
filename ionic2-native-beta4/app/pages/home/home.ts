994ecd2845c010b21da20aaa1097126a0677d824
Koma <admin@osser.jp>
2016-07-23 13:25:04 +0900

    ionic2-native-beta4:更新

--- ionic2-native-beta4/app/pages/home/home.ts
+++ ionic2-native-beta4/app/pages/home/home.ts
@@ -1,11 +1,29 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+import { Screenshot, InAppBrowser } from 'ionic-native';
+
 @Component({
     templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+
+    msg: any;
+
     constructor(private navCtrl: NavController) {
 
+    }
+
+    captureScreen() {
+        Screenshot.save("jpg", 100, "test").then((result) => {
+            if (result) {
+                this.msg = JSON.stringify(result);
+                console.error(result);
+            }
+        });
+    }
+
+    openGoogle() {
+        InAppBrowser.open("http://www.google.co.jp", "_blank", "location=no");
     }
 }
